d066781479d3522db5ba73d095956e2669a9ce56
Russ Bain <phaltak-2@yahoo.com>
2018-03-18 23:13:54 -0700

    added amazon disclaimer

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,21 +1,41 @@
 <template>
   <f7-page>
     <!--<f7-navbar title="About" back-link="Back"></f7-navbar>-->
-    <f7-navbar  back-link="Back"></f7-navbar>
-    <f7-block-title>About this app</f7-block-title>
+    <f7-navbar title="About This App" back-link="Back"></f7-navbar>
+    <banner-ad-top></banner-ad-top>
     <f7-block strong>
 
       <p>I built this simple single page app to raise funds for my Hack Reactor expenses</p>
       <p>
         It was built on a backbone of Webpack, VueJS, and Framework 7
-
+      </p>
+      <p>
+        Russ Bain is a participant in the Amazon Services LLC Associates Program,
+        an affiliate advertising program designed to provide a means for sites
+        to earn advertising fees by advertising and linking to Amazon.com.
       </p>
 
-      <f7-link back>back</f7-link>
+
+    </f7-block>
+    <f7-block>
+      <banner-ad-bottom></banner-ad-bottom>
     </f7-block>
   </f7-page>
 </template>
 
 <script>
-export default {}
+
+  import BannerAdTop from './BannerAdTop.vue';
+  import BannerAdBottom from './BannerAdBottom.vue';
+  import BannerAdAmazon from './BannerAdLarge.vue';
+
+  export default {
+    components: {
+      BannerAdAmazon,
+      BannerAdBottom,
+      BannerAdTop
+    }
+
+
+  }
 </script>
